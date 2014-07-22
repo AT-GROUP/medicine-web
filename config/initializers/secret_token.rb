d73ecaf683882a08ca2c3699012b76453b3f5355
Elshiko <Minak.S@mail.ru>
2014-07-22 19:16:11 +0400

    empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MedicineWeb::Application.config.secret_key_base = '7700706aefecc6ce573bdc630f3c0f6885227794dd6393ecf0d7955a15ce5a36aa87f27fe07ee0474a12e37f0dd5a3b459c497525603ca13d4c75d80771c89b8'
